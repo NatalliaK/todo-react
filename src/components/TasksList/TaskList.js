b1c591a85c48288d2a600283b3d7f22242f5885b
NatalliaK <studina-natali@yandex.by>
2018-08-03 12:02:32 +0300

    окончательная версия1

--- src/components/TasksList/TaskList.js
+++ src/components/TasksList/TaskList.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-
-import taskList from '../../decorators/taskList';
 import TaskHeader from './TaskHeader';
 import TaskItem from './TaskItem';
 
-class TaskList extends Component {
-	render() {
-		return (
-			<table className="table">
-			<TaskHeader sortTasks={this.props.sortTasks} />
-			<tbody>
-			{this.props.tasks.map((elem, i) =>
-				<TaskItem key={elem.id}{...elem} toggleDone={this.props.toggleDone} defaultChecked={elem.done} />
-			)}
-			</tbody>
-		</table>
-		)
-	}
+export default class TaskList extends Component {
+  render() {
+    return (
+      <table className="table">
+        <TaskHeader sortTasks={this.props.sortTasks} />
+        <tbody>
+          {this.props.tasks.map((elem, i) => (
+            <TaskItem
+              key={elem.id}
+              {...elem}
+              toggleDone={this.props.toggleDone}
+              defaultChecked={elem.done}
+            />
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
-
-export default taskList(TaskList);
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,24 @@
 let list = [];
 
 export function addTask(task) {
-	let newTask = {
-		id: Date.now(),
-		done: false,
-		taskByDate: true,
-		...task
-	};
+  let newTask = {
+    id: Date.now(),
+    done: false,
+    taskByDate: true,
+    search: false,
+    ...task
+  };
 
-	list.push(newTask);
+  list.push(newTask);
 
-	let promise = new Promise(resolve => {
-		resolve(localStorage.setItem(`tasks`, JSON.stringify([...list])));
-	});
+  let promise = new Promise(resolve => {
+    resolve(localStorage.setItem(`tasks`, JSON.stringify([...list])));
+  });
 
-	return promise.then(_ => Promise.resolve(newTask));
+  return promise.then(_ => Promise.resolve(newTask));
 }
 
 export function getTasks() {
-	list = JSON.parse(localStorage.getItem('tasks')) || [];
-	return Promise.resolve([...list]);
+  list = JSON.parse(localStorage.getItem('tasks')) || [];
+  return Promise.resolve([...list]);
 }
